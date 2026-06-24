7c8eb1bbb5dd378b6a76000ef4353735660e130d
yuvaantusuju <ytusuju@gmail.com>
2026-06-25 00:04:54 +0530

    Initial commit.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
+initOpenNextCloudflareForDev();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Images from external domains
